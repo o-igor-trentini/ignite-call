dd09c8b4d8292391b2bdf708f00ba5088c28b4f2
Igor Trentini <igortrentini.2004@gmail.com>
2023-03-05 12:48:52 -0300

    feat(register/form): Validações e exibição de erro do formulário

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,9 +1,40 @@
 import { FC } from 'react'
-import { Form, Header, RegisterContainer } from './styles'
+import { Form, FormError, Header, RegisterContainer } from './styles'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { z } from 'zod'
+import { FieldError, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const registerFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((value) => value.toLowerCase()),
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa ter pelo menos 3 letras.' }),
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
 
 const Register: FC = () => {
+  const { register, handleSubmit, formState } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerFormSchema),
+  })
+
+  const errors = formState.errors
+
+  const handleRegister = async (values: RegisterFormData): Promise<void> => {
+    console.log(values)
+  }
+
+  const formatError = (error?: FieldError) =>
+    error ? <FormError size="sm">{error.message}</FormError> : <></>
+
   return (
     <RegisterContainer>
       <Header>
@@ -17,17 +48,30 @@ const Register: FC = () => {
         <MultiStep size={4} currentStep={1} />
       </Header>
 
-      <Form as="form">
+      <Form as="form" onSubmit={handleSubmit(handleRegister)}>
         <label>
           <Text size="sm">Nome de usuário</Text>
 
-          <TextInput prefix="ignite.com/" placeholder="Seu usuário" />
+          <TextInput
+            prefix="ignite.com/"
+            placeholder="Seu usuário"
+            autoComplete="off"
+            {...register('username')}
+          />
+
+          {formatError(errors.username)}
         </label>
 
         <label>
           <Text size="sm">Nome completo</Text>
 
-          <TextInput placeholder="Seu nome" />
+          <TextInput
+            placeholder="Seu nome"
+            {...register('name')}
+            autoComplete="off"
+          />
+
+          {formatError(errors.name)}
         </label>
 
         <Button type="submit">
